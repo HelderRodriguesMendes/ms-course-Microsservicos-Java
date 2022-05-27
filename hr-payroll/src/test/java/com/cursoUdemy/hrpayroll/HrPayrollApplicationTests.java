35a06dc28c8df8a3d1455c269c92dea77854d931
helde <helderrodrigues37@gmail.com>
2022-05-26 21:26:50 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/com/cursoUdemy/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoUdemy.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
